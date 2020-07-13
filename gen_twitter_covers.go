193fa33569af8bc028ff91253e4fef3a45b56fc8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-07-12 17:30:19 -0700

    add code to generate twitter covers and optimize images

---
+++ gen_twitter_covers.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"image"
+	"image/png"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/kjk/u"
+	"golang.org/x/image/draw"
+)
+
+func loadImageMust(path string) image.Image {
+	r, err := os.Open(path)
+	u.PanicIfErr(err)
+	defer r.Close()
+	img, _, err := image.Decode(r)
+	u.PanicIfErr(err)
+	return img
+}
+
+func saveImageAsPNGMust(path string, img image.Image) {
+	w, err := os.Create(path)
+	u.PanicIfErr(err)
+	defer func() {
+		err = w.Close()
+		u.PanicIfErr(err)
+	}()
+	err = png.Encode(w, img)
+	u.PanicIfErr(err)
+}
+
+func optiImageMust(path string) {
+	cmd := exec.Command("optipng", "-o7", path)
+	_, err := cmd.CombinedOutput()
+	u.PanicIfErr(err)
+}
+
+func saveImageAsPNGAndOptimize(path string, img image.Image) {
+	saveImageAsPNGMust(path, img)
+	optiImageMust(path)
+	logf("Saved and optimized '%s'\n", path)
+}
+
+func getSubimage(img image.Image, r image.Rectangle) image.Image {
+	switch im := img.(type) {
+	case *image.NRGBA:
+		return im.SubImage(r)
+	case *image.Paletted:
+		return im.SubImage(r)
+	}
+	u.PanicIf(true, "unsupported image type %T", img)
+	return img
+}
+
+func genTwitterImage(img image.Image) image.Image {
+	r := img.Bounds()
+	dx := r.Dx()
+	r = image.Rectangle{
+		Min: image.Point{0, 0},
+		Max: image.Point{dx, dx},
+	}
+	return getSubimage(img, r)
+}
+
+func printImageInfo(path string, img image.Image) {
+	r := img.Bounds()
+	logf("%s: %v %T\n", path, r, img)
+}
+
+func getExistingImagesMust(dir string) map[string]bool {
+	m := make(map[string]bool)
+	fileInfos, err := ioutil.ReadDir(dir)
+	u.PanicIfErr(err)
+	for _, fi := range fileInfos {
+		name := fi.Name()
+		ext := strings.ToLower(filepath.Ext(name))
+		if ext == ".png" {
+			m[name] = true
+		}
+	}
+	return m
+}
+
+// returns a list of cover images (.png files that are not @2x versions)
+func getCoversListMust(dir string) []string {
+	var res []string
+	fileInfos, err := ioutil.ReadDir(dir)
+	u.PanicIfErr(err)
+	for _, fi := range fileInfos {
+		name := fi.Name()
+		ext := strings.ToLower(filepath.Ext(name))
+		if ext != ".png" {
+			continue
+		}
+		if strings.Contains(name, "@2x") {
+			continue
+		}
+		res = append(res, name)
+	}
+	return res
+}
+
+func genTwitterImagesAndExit() {
+	srcDir := "covers"
+	dstDir := filepath.Join(srcDir, "twitter")
+	u.CreateDirMust(dstDir)
+	covers := getCoversListMust(srcDir)
+	existingImages := getExistingImagesMust(dstDir)
+	// logf("covers: %v\n", covers)
+	for _, coverName := range covers {
+		dstPath := filepath.Join(dstDir, coverName)
+		if _, ok := existingImages[coverName]; false && ok {
+			logf("%s already exists as %s\n", coverName, dstPath)
+			continue
+		}
+		path := filepath.Join(srcDir, coverName)
+		img := loadImageMust(path)
+		printImageInfo(path, img)
+		sub := genTwitterImage(img)
+		saveImageAsPNGAndOptimize(dstPath, sub)
+	}
+	os.Exit(0)
+}
+
+func resize(src image.Image, dstSize image.Point) *image.RGBA {
+	srcRect := src.Bounds()
+	dstRect := image.Rectangle{
+		Min: image.Point{0, 0},
+		Max: dstSize,
+	}
+	dst := image.NewRGBA(dstRect)
+	draw.CatmullRom.Scale(dst, dstRect, src, srcRect, draw.Over, nil)
+	return dst
+}
+
+func getProportionalY(p image.Point, x int) int {
+	res := (int64(p.Y) * int64(x)) / int64(p.X)
+	return int(res)
+}
+
+func genSmallCoverImage(img image.Image) *image.RGBA {
+	size := img.Bounds().Size()
+	x := 140
+	y := getProportionalY(size, x)
+	return resize(img, image.Point{x, y})
+}
+
+// this is one-time code to generate 140 x 198 small images from 595 x 842
+// source images. Small images are used in
+func genSmallCoversAndExit() {
+	srcDir := "covers"
+	dstDir := filepath.Join(srcDir, "covers_small")
+	u.CreateDirMust(dstDir)
+	covers := getCoversListMust(srcDir)
+	existingImages := getExistingImagesMust(dstDir)
+	// logf("covers: %v\n", covers)
+	for _, coverName := range covers {
+		dstPath := filepath.Join(dstDir, coverName)
+		if _, ok := existingImages[coverName]; false && ok {
+			logf("%s already exists as %s\n", coverName, dstPath)
+			continue
+		}
+		path := filepath.Join(srcDir, coverName)
+		img := loadImageMust(path)
+		printImageInfo(path, img)
+		sub := genSmallCoverImage(img)
+		saveImageAsPNGAndOptimize(dstPath, sub)
+	}
+	os.Exit(0)
+
+}
---
+++ optimize_images.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// run optipng in parallel
+func optimizeWithOptipng(path string) {
+	logf("Optimizing '%s'\n", path)
+	cmd := exec.Command("optipng", "-o5", path)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		// it's ok if fails. some jpeg images are saved as .png
+		// which trips it
+		logf("optipng failed with '%s'\n", err)
+	}
+}
+
+func maybeOptimizeImage(path string) {
+	ext := filepath.Ext(path)
+	ext = strings.ToLower(ext)
+	switch ext {
+	// TODO: for .gif requires -snip
+	case ".png", ".tiff", ".tif", "bmp":
+		optimizeWithOptipng(path)
+	}
+}
+
+func optimizeAllImages() {
+	// verify we have optipng installed
+	cmd := exec.Command("optipng", "-h")
+	err := cmd.Run()
+	panicIf(err != nil, "optipng is not installed")
+
+	dirsToVisit := []string{"."}
+	for len(dirsToVisit) > 0 {
+		dir := dirsToVisit[0]
+		dirsToVisit = dirsToVisit[1:]
+		files, err := ioutil.ReadDir(dir)
+		must(err)
+		for _, f := range files {
+			name := f.Name()
+			path := filepath.Join(dir, name)
+			if f.IsDir() {
+				if name == "www" || name == "covers" || name == "tmpl" {
+					// assume already optimized
+				} else {
+					dirsToVisit = append(dirsToVisit, path)
+				}
+				continue
+			}
+			maybeOptimizeImage(path)
+		}
+	}
+}
